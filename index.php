4d5268596123d072c54bcaa095c3d0fa2cfc916c
malhuneidi <malhuneidi@unomaha.edu>
2017-02-09 23:17:06 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+<title>Runscape Gamble Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1 {font-family: "Raleway", Arial, sans-serif}
+h1 {letter-spacing: 6px}
+.w3-row-padding img {margin-bottom: 12px}
+</style>
+<body>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-content" style="max-width:1500px">
+
+<!-- Header -->
+<header class="w3-panel w3-padding-128 w3-center w3-opacity">
+  <h1 class="w3-xlarge">Runescape Jackpot</h1>
+  <h1>Gamble/Sell/Buy RSGP</h1>
+  
+  <div class="w3-padding-32">
+    <div class="w3-btn-bar w3-border w3-show-inline-block">
+      <a href="#" class="w3-btn">Info</a>
+      <a href="#" class="w3-btn w3-light-grey">Gamble</a>
+      <a href="#" class="w3-btn">Buy RS Gold</a>
+      <a href="#" class="w3-btn w3-hide-small">Sell RS Gold</a>
+    </div>
+  </div>
+</header>
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-light-grey w3-center"> 
+  <a href="#" class="w3-hover-text-indigo"><i class="fa fa-facebook-official"></i></a>
+  <a href="#" class="w3-hover-text-red"><i class="fa fa-pinterest-p"></i></a>
+  <a href="#" class="w3-hover-text-light-blue"><i class="fa fa-twitter"></i></a>
+  <a href="#" class="w3-hover-text-grey"><i class="fa fa-flickr"></i></a>
+  <a href="#" class="w3-hover-text-indigo"><i class="fa fa-linkedin"></i></a>
+  <p>Powered by Mo's Amazon Web Service Servr</p>
+</footer>
+
+</body>
+</html>
+
+
